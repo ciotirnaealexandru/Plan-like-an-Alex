d2b6eeed4fe7566dfc52ef23e4432f9a6494f324
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-03-05 21:40:30 +0200

    Fix signup route
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="signup" element={<SignUp />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 }
