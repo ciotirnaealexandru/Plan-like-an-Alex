31445e5f809ff68da31d97f80d8ae81076172530
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-03-06 23:52:27 +0200

    Clear backend and add users module boilerplate
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksController } from './tasks/tasks.controller';
-import { TasksService } from './tasks/tasks.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, TasksController],
-  providers: [AppService, TasksService],
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
