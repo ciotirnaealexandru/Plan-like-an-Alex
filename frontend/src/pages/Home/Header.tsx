f56ab7847307c2427ad088d262a83861257c9516
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-03-12 22:31:51 +0200

    Add functional date
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- frontend/src/pages/Home/Header.tsx
+++ frontend/src/pages/Home/Header.tsx
@@ -2,12 +2,42 @@ import ProfilePicture from "../../images/profile_picture.jpeg";
 import "./Header.scss";
 
 const Header = () => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let now = new Date();
+  let weekDay = days[now.getDay()];
+  let day = now.getDate();
+  let month = months[now.getMonth()];
+  let dateFormat = day + " " + month;
+
   return (
     <>
       <div className="flex justify-between items-center">
         <div className="flex flex-col">
-          <div className="text-gray-300 text-xl">Monday</div>
-          <div className="text-3xl font-bold">25 October</div>
+          <div className="text-gray-300 text-xl">{weekDay}</div>
+          <div className="text-3xl font-bold">{dateFormat}</div>
         </div>
         <div className="w-15 h-15 rounded-xl bg-gradient-to-br from-red-600 via-purple-300 to-green-600">
           <div className="bg-white m-0.75 rounded-xl">
