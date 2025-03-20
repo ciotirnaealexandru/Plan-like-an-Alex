e01473044dafdcff99e6ed498fb45597d86c1614
Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>
2025-03-20 20:30:52 +0200

    Normalize CLIENT_URL to remove trailing slash
    Signed-off-by: Ciotirnae Alexandru-Cosmin <ciotirnae.alexandru005@gmail.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CLIENT_URL || 'http://localhost:3000',
+    origin:
+      process.env.CLIENT_URL?.replace(/\/$/, '') || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
